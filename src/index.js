35af95799b0ae1906d5cc06345db128432b995c0
Andrei Shauko <andrevvise@gmail.com>
2020-10-02 10:12:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    if (n[0] === "-") n = n.slice(1);
+    let result ="";
+    for (let i = 0; i < n.length; i++) {
+        result = n[i] + result;
+    }
+  return Number(result);
 }
